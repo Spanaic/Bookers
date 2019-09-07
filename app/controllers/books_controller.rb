39326f5b6a0fca86d5c11019e773be449b054f6f
Takeru Yamanaka <Takeru@takeru-no-MacBook-Pro-Retina.local>
2019-09-07 15:56:43 +0900

    [基本的な機能の実装は完了。これからレイアウトを整える]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,10 @@ class BooksController < ApplicationController
       # redirect_to book_path(book.id)
       # 今だけ一覧ページにリダイレクト中
       # if　と同じ行に条件を記述することを忘れない
-      redirect_to books_path
+
+      flash[:notice] = 'Book was successfully created.'
+
+      redirect_to book_path(@book)
     else
       render action: :index
       # コントローラ内のレンダーは指定したアクションの"end"から実行されるため、変数は現在のアクション内で再定義する必要がある
@@ -24,9 +27,30 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
+
+      flash[:notice] = 'Book was successfully updated.'
+
+    redirect_to book_path
+   else
+    render action: :edit 
+   end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    # []を｛｝と間違えない
+    book.destroy
+    redirect_to books_path
   end
 
   private
